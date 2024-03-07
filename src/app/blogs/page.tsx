c5d28d7313d7b7b7684d01c9ca0ba73da536e694
Patrick Louis <patricklouis@Patricks-MacBook-Pro.local>
2024-03-07 17:50:04 -0500

    starting to update blogs page

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+const dummy_data = [
+  {
+    title: "Test",
+    content:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex hic esse eos cumque ipsam praesentium dolorum eveniet, saepe tempora. Voluptatem voluptatibus quibusdam non nam temporibus labore. Non hic aliquid accusantium.",
+  },
+];
+
 const Blogs = () => {
   return <div>Blogs</div>;
 };
