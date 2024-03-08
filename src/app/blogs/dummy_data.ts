5462cfa8f944e52d3f0e4fdb26efe4e971945f63
Patrick Louis <patricklouis@Patricks-MacBook-Pro.local>
2024-03-07 21:01:05 -0500

    styling blogs page

---
+++ src/app/blogs/dummy_data.ts
@@ -0,0 +1,47 @@
+export const dummy_data = [
+    {
+      id: 1,
+      title: "Test",
+      content:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex hic esse eos cumque ipsam praesentium dolorum eveniet, saepe tempora. Voluptatem voluptatibus quibusdam non nam temporibus labore. Non hic aliquid accusantium.",
+      date_published: "Jan 4 2024",
+      picture:
+        "https://images.pexels.com/photos/1658967/pexels-photo-1658967.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 2,
+      title: "Test 1",
+      content:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex hic esse eos cumque ipsam praesentium dolorum eveniet, saepe tempora. Voluptatem voluptatibus quibusdam non nam temporibus labore. Non hic aliquid accusantium.",
+      date_published: "Jan 4 2024",
+      picture:
+        "https://images.pexels.com/photos/1658967/pexels-photo-1658967.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 3,
+      title: "Test 2",
+      content:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex hic esse eos cumque ipsam praesentium dolorum eveniet, saepe tempora. Voluptatem voluptatibus quibusdam non nam temporibus labore. Non hic aliquid accusantium.",
+      date_published: "Jan 4 2024",
+      picture:
+        "https://images.pexels.com/photos/1658967/pexels-photo-1658967.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 4,
+      title: "Test 3",
+      content:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex hic esse eos cumque ipsam praesentium dolorum eveniet, saepe tempora. Voluptatem voluptatibus quibusdam non nam temporibus labore. Non hic aliquid accusantium.",
+      date_published: "Jan 4 2024",
+      picture:
+        "https://images.pexels.com/photos/1658967/pexels-photo-1658967.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 5,
+      title: "Test 4",
+      content:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex hic esse eos cumque ipsam praesentium dolorum eveniet, saepe tempora. Voluptatem voluptatibus quibusdam non nam temporibus labore. Non hic aliquid accusantium.",
+      date_published: "Jan 4 2024",
+      picture:
+        "https://images.pexels.com/photos/1658967/pexels-photo-1658967.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+  ];
